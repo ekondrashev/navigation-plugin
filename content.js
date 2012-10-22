7f5edefbd045460b6a0ea0e50769a06489c3fa52
ekondrashev <eugene.kondrashev@gmail.com>
2012-10-22 22:31:51 +0300

    parsed 'a' elements

--- content.js
+++ content.js
@@ -1,4 +1,15 @@
 //alert('Hey!');
+
 $('document').ready(function(){
 	$('body').append('<div id="navigation-tree"></div>');
+	var navigationTree = $('#navigation-tree');
+	
+	
+	navigationTree.append('<h1 id="nt-header">'+$('#header').text()+'</h1>');
+	$('#table tr:gt(0)').each(function(){
+		navigationTree.append($(this).children('td:eq(0)').html());
+	});
+	
+	
+
 });
